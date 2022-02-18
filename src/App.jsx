681982401530d681d5b6947fd59c2d7101c9c870
poliana <poliana.karenn05@gmail.com>
2022-02-18 16:26:03 -0300

    criando os 4 estados do App e o input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+// Importando módulos
+import React, { useState } from 'react';
+
+// Teremos 4 estados:
+// pokemon: estado que armazenará o resultado da busca na API;
+// typedPokemon: estado responsável pelo input;
+// error: estado responsável pelos erros;
+// isLoading: estado responsável pelo carregamento.
 
 const App = () => {
-    return (
-        <div>
-            <h1>Olá mundo</h1>
-        </div>
-    );
+    const [pokemon, setPokemon] = useState(null);
+    const [error, setError] = useState(null);
+    const [typedPokemon, setTypedPokemon] = useState('');
+    const [isLoading, setLoading] = useState(false);
+
+    // elemento de input
+    const handleChange = (event) => {
+        setTypedPokemon(event.target.value);
+    };
 };
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+// Services: é onde mantemos nossas configurações de chamada à API;
+
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://pokeapi.com/api/v2'
+});
+
+export default api;
